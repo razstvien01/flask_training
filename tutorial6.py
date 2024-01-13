a609e4964d9685f5e2979e649a27c98604167eac
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-01-13 22:18:47 +0800

    added login.html and message flash functions

--- tutorial6.py
+++ tutorial6.py
@@ -1,4 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+"""
+? Message Flashing
+* - showing some kind of information from a previous page on the next page
+"""
+
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -15,13 +20,15 @@ def home():
 @app.route("/login", methods=["POST", "GET"])
 def login():
   if request.method == "POST":
-    # session.permanent = True #* Permanent sessionn
+    session.permanent = True #* Permanent sessionn
     user = request.form["nm"]
     session["user"] = user
     
+    flash("Login Successful!")
     return redirect(url_for("user"))
   else:   
     if "user" in session:
+      flash("Already Logged In!")
       return redirect(url_for("user"))
     
     return render_template("login.html")
@@ -31,12 +38,18 @@ def user():
   if "user" in session:
     user = session["user"]
     
-    return f"<h1>{user}</h1>"
+    return render_template("user.html", user=user)
   else:
+    flash("You are not logged in!")
     redirect(url_for("login"))
     
 @app.route("/logout")
 def logout():
+  
+  
+  if "user" in session:
+    user = session["user"]
+    flash(f"You have been logged out, {user}", "info")
   session.pop("user", None)
   return redirect(url_for("login"))
 
