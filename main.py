300d75bd9d4636195f8fd70eae5b32dab9cead13
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-01-17 12:01:22 +0800

    made a folder for the admin blueprint

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+"""
+* Blueprints
+* Allows us to divide our application into separate Python files where we can actually pass specific views and render templates from different areas of our kind of project and application
+"""
 from flask import Flask, render_template
+from admin.second import second
 
 app = Flask(__name__)
 
-@app.route("/home")
+# * Url prefix is what needs to come first to send something to that blueprint
+app.register_blueprint(second, url_prefix="/admin")
+
 @app.route("/")
-def home():
-  return render_template("home.html")
+def test():
+  return "<h1>Test</h1>"
 
 if __name__ == "__main__":
   app.run(debug=True)
